1f49f370b6bf40990922ec32de763704b11a2730
Paquet--Kremer Robin <robin.paquet-kremer3@etu.univ-lorraine.fr>
2025-06-03 16:28:51 +0200

    REFACTOR

--- main.test.ts
+++ main.test.ts
@@ -75,9 +75,26 @@ function conversionVersMatriceNumerique(minesweeperString: string[][]): number[]
         tableConversionTemporaire = []
     }
 
-    console.log(matriceNumerique)
+    //console.log(matriceNumerique)
     return matriceNumerique
 
+}
+
+function conversionString(minesweeperNumerique: number[][]): string {
+    let champNumero = ""
+
+    for(let tableActuelle of minesweeperNumerique) {
+        for(let element of tableActuelle) {
+            if(element === -1) {
+                champNumero += "*"
+            } else {
+                champNumero += element.toString()
+            }
+        }
+        champNumero += "\n"
+    }
+
+    return champNumero.slice(0, champNumero.length-1)
 }
 
 function minesweeper(champ: string) {
@@ -87,63 +104,8 @@ function minesweeper(champ: string) {
     //console.log(champMatrice)
 
     let matriceConversion = conversionVersMatriceNumerique(champMatrice)
-    // let matriceConversion: number[][] = []
-    // let tableConversionTemporaire: number[] = []
 
-    // //on parcours une matrice donc j'utilise x, y comme un plan en 2 dimension
-    // for (let y = 0; y < champMatrice.length; y++) {
-    //     //console.log(champMatrice.length)
-    //     for (let x = 0; x < champMatrice[y].length; x++) {
-    //         //console.log(champMatrice[y].length)
-    //         if (champMatrice[y][x] === ".") {
-    //             //console.log(champTable[i])
-
-    //             tableConversionTemporaire.push(0)
-    //             //si il existe une ligne au dessus on teste les valeurs adjacante
-    //             if(champMatrice[y-1]){
-    //                 if(champMatrice[y-1][x-1] === "*") {
-    //                     tableConversionTemporaire[x] += 1
-    //                 }
-    //                 if(champMatrice[y-1][x] === "*") {
-    //                     tableConversionTemporaire[x] += 1
-    //                 }
-    //                 if(champMatrice[y-1][x+1] === "*") {
-    //                     tableConversionTemporaire[x] += 1
-    //                 }
-    //             }
-                
-    //             if (champMatrice[y][x + 1] === "*") {
-    //                 tableConversionTemporaire[x] += 1
-    //             }
-                
-    //             if (champMatrice[y][x - 1] === "*") {
-    //                 tableConversionTemporaire[x] += 1
-    //             }
-
-    //             //si il existe une ligne au dessous on teste les valeurs adjacante
-    //             if(champMatrice[y+1]) {
-    //                 if(champMatrice[y+1][x-1] === "*") {
-    //                     tableConversionTemporaire[x] += 1
-    //                 }
-    //                 if(champMatrice[y+1][x] === "*") {
-    //                     tableConversionTemporaire[x] += 1
-    //                 }
-    //                 if(champMatrice[y+1][x+1] === "*") {
-    //                     tableConversionTemporaire[x] += 1
-    //                 }
-    //             }
-
-    //         } else if (champMatrice[y][x] === "*") {
-    //             tableConversionTemporaire.push(-1);
-    //         }
-    //     }
-    //     matriceConversion.push(tableConversionTemporaire)
-    //     tableConversionTemporaire = []
-    // }
-
-    // console.log(matriceConversion)
-
-
+    return conversionString(matriceConversion)
 
     let champNumero = ""
 
