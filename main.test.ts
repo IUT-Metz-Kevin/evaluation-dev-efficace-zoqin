f3a5f23b64ecf18055f90abf2e0ce025fa913ee3
Paquet--Kremer Robin <robin.paquet-kremer3@etu.univ-lorraine.fr>
2025-06-03 14:11:04 +0200

    REFACTOR

--- main.test.ts
+++ main.test.ts
@@ -1,6 +1,18 @@
 import { assertEquals } from "jsr:@std/assert";
 function minesweeper(champ: string) {
     let champNumero = ""
+    for(let i = 0; i < champ.length; i++) {
+        if(champ[i] === ".") {
+            champNumero += "0";
+        } else if(champ[i] === "*") {
+            champNumero += "*";
+        } else {
+            champNumero += "\n";
+        }
+    }
+
+    return champNumero
+
     for(let c of champ) {
         if(c === ".") {
             champNumero += "0";
